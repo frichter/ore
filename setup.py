d47c53c0b461183cd54ade7a78f54a0c686cabc7
frichter <frichter91@gmail.com>
2018-04-22 15:18:25 -0400

    https://pypi.org/help/#file-name-reuse

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.rst", "rb") as f:
 
 setup(name='ore',
       packages=['ore'],
-      version='0.1.0',
+      version='0.1.1',
       description='Associate outliers with rare variation',
       long_description=long_descr,
       entry_points={
