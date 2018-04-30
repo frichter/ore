48442729749bd28ab135c6777c4992bf2963ddc7
frichter <frichter91@gmail.com>
2018-04-30 13:56:12 -0400

    fewer MP

--- ore_bsub.sh
+++ ore_bsub.sh
@@ -52,7 +52,7 @@ python -m ore.ore --vcf $VCF \
     --ensgene \
     --refgene \
     --humandb_dir "/sc/orga/projects/chdiTrios/whole_genome/humandb" \
-    --processes 12
+    --processes 6
 
 
 
