b65256bb6c1c969d4135af6d5c66c2db2972aa3f
frichter <frichter91@gmail.com>
2018-05-01 16:28:23 -0400

    updated joining and enrichment

--- ore/annotations.py
+++ ore/annotations.py
@@ -272,8 +272,10 @@ class Annotations(object):
             final_df['popmax_af'] = 0
             final_df["annovar_func"] = "NA"
         print("Writing to", self.final_var_loc % current_chrom)
+        final_df.var_id_count.head()
+        final_df.var_id_freq.head()
         print("var_id_freq columns:", final_df[
-            ['var_id_count', 'var_id_freq'].head()])
+            ['var_id_count', 'var_id_freq']].head())
         print("Final DF being written size:", final_df.shape)
         final_df.to_csv(self.final_var_loc % current_chrom, sep="\t",
                         index=False)
