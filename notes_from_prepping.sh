120089604b8b6173c0ea1c5a88580102bec1566d
frichter <frichter91@gmail.com>
2018-07-01 13:32:27 -0400

    fixed chr bedtools issue

--- notes_from_prepping.sh
+++ notes_from_prepping.sh
@@ -322,7 +322,7 @@ time mprof run --include-children --multiprocess python -m ore.ore --vcf $VCF \
     --ensgene \
     --refgene \
     --humandb_dir "/sc/orga/projects/chdiTrios/whole_genome/humandb" \
-    --processes 6
+    --processes 2
 
     # --outlier_output "outliers_norm_SV5.txt" \
 
