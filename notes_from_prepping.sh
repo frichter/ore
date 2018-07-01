252c15094b33bc573cdfc23a5c24283ce3162d60
frichter <frichter91@gmail.com>
2018-07-01 17:27:01 -0400

    annotation processing improvements

--- notes_from_prepping.sh
+++ notes_from_prepping.sh
@@ -322,7 +322,7 @@ time mprof run --include-children --multiprocess python -m ore.ore --vcf $VCF \
     --ensgene \
     --refgene \
     --humandb_dir "/sc/orga/projects/chdiTrios/whole_genome/humandb" \
-    --processes 6
+    --processes 2
 
 
 
