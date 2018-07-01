22eed85a6653c62b8b70138a5774e1f595d3bc50
frichter <frichter91@gmail.com>
2018-07-01 09:39:09 -0400

    updated column naming

--- ore/genes.py
+++ ore/genes.py
@@ -177,8 +177,6 @@ class Genes(object):
                                header=None,
                                usecols=cols_to_use,
                                names=bed_col_names)
-        print(var_df.head())
-        print(var_df.shape)
         # max TSS distance
         var_df = var_df.loc[abs(var_df.tss_dist) <= max_tss_dist]
         if upstream_only:
@@ -188,6 +186,7 @@ class Genes(object):
         # var_id should use 1-based start position
         var_df = var_df.assign(Start1b=var_df.Start + 1)
         # var_df["Start1b"] = var_df.Start + 1
+        print("Start 1b should be added")
         print(var_df.head())
         print(var_df.shape)
         var_df["var_id"] = (var_df.Chrom.str.cat(var_df.Start1b.
