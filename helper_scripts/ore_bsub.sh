ad991a8210caa85258ee7e1ede88d0e831a2c065
frichter <frichter91@gmail.com>
2018-07-16 10:03:59 -0400

    moved helper scripts

---
+++ helper_scripts/ore_bsub.sh
@@ -0,0 +1,55 @@
+#BSUB -W 02:00
+#BSUB -q expressalloc
+#BUSB -n 8
+#BSUB -R "rusage[mem=10000]"
+#BSUB -P acc_chdiTrios
+#BSUB -J ad_ore
+#BSUB -m mothra
+#BSUB -o ad_ore.stdout
+#BSUB -e ad_ore.stderr
+
+module load bedtools/2.27.0
+module load samtools/1.3
+module load bcftools/1.6
+module load python/3.5.0
+module load py_packages/3.5
+
+######################### AD Tissue 36 #########################
+
+# cd /sc/orga/projects/chdiTrios/Felix/alzheimers
+
+PARENT_DIR="/sc/orga/projects/chdiTrios/Felix/alzheimers"
+EXPR_F="$PARENT_DIR/expression/residuals_AMPAD_MSSM_GE_SV_17_tissue_36_with_disease_in_model_europeans_only_new_z_wgs_ids.bed.gz"
+VCF="$PARENT_DIR/wgs/ad_wgs_cp.vcf.gz"
+# ore_2018_06 ore_2018_05
+OUT_PREFIX="$PARENT_DIR/ore_2018_06_job/ad_ore"
+OUTLIER_OUT="$PARENT_DIR/ore_2018_05/most_extreme_outs_t36/ad_ore_outliers.txt"
+ENRICH_F="$PARENT_DIR/ore_2018_05/most_extreme_t36_enrich/ad_ore_upstream_ref_10kb.txt"
+
+cd /sc/orga/projects/chdiTrios/Felix/dna_rna/ore
+
+# upstream and downstream (together) all annovar or subset
+# ENRICH_F=$ENRICH_PREFIX"/tssBi_SV5_norm_ncRNA_refgene_ensgene.txt"
+python -m ore.ore --version
+
+time mprof run --include-children --multiprocess python -m ore.ore --vcf $VCF \
+    --bed $EXPR_F \
+    --output $OUT_PREFIX \
+    --outlier_output $OUTLIER_OUT \
+    --enrich_file $ENRICH_F \
+    --distribution "normal" \
+    --extrema \
+    --threshold 2 \
+    --af_rare 5e-2 1e-2 1e-3 1e-4 1e-5 \
+    --intracohort_rare_ac 5 \
+    --tss_dist 1e3 2e3 5e3 1e4 \
+    --annovar \
+    --refgene \
+    --variant_class "upstream" \
+    --humandb_dir "/sc/orga/projects/chdiTrios/whole_genome/humandb" \
+    --processes 5
+
+
+
+# cd /sc/orga/projects/chdiTrios/Felix/alzheimers
+
