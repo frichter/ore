ef10588960bdb6c47bfd3793320b0c32b3cf8303
frichter <frichter91@gmail.com>
2018-07-17 11:51:46 -0400

    updated AD bsub commands

--- ore/join_dna_rna.py
+++ ore/join_dna_rna.py
@@ -173,11 +173,17 @@ class JoinedVarExpr(object):
         if refgene:
             vars_refgene = var_df_per_chrom.exon_func_refgene.str.contains(
                 exon_class, regex=True)
+            print(var_df_per_chrom.head)
+            print(var_df_per_chrom.shape)
             var_df_per_chrom = var_df_per_chrom[vars_refgene]
+            print(var_df_per_chrom.head)
+            print(var_df_per_chrom.shape)
         if ensgene:
             vars_ensgene = var_df_per_chrom.exon_func_ensgene.str.contains(
                 exon_class, regex=True)
             var_df_per_chrom = var_df_per_chrom[vars_ensgene]
+            print(var_df_per_chrom.head)
+            print(var_df_per_chrom.shape)
         return var_df_per_chrom
 
     def load_outliers(self, expr_outs_loc):
