f306e07a83c913b91170e4f03e8cc9fb63c807cf
frichter <frichter91@gmail.com>
2018-07-19 14:23:48 -0400

    added plotting helper scripts

---
+++ ore/helper_scripts/plot_enrichment_helper.R
@@ -0,0 +1,64 @@
+# Felix Richter (felix.richter@mssm.edu)
+# Plot different combinations of enrichment
+#############################################
+
+options(stringsAsFactors=FALSE)
+p = c("magrittr", "purrr", "dplyr", "ggplot2", "tidyr", "readr")
+
+## Run once: install packages
+lapply(p, install.packages)
+
+## Load packages
+lapply(p, require, character.only = TRUE)
+
+## optionally, install interesting color pallettes
+library(wesanderson)
+
+## enrichment file directory
+enrich_file_dir = "/Users/felixrichter/Dropbox/PhD/alzheimers/enrichment"
+
+## Load data
+enrich_file_list = list.files(enrich_file_dir, ".*txt", full.names = T)
+## clean names of enrichment runs
+names(enrich_file_list) = gsub(".*/", "", enrich_file_list) %>% gsub(".txt$", "", .) %>% 
+  gsub("ad_ore_", "", .) %>% gsub("_10kb", "", .)
+enrich_df = map_df(enrich_file_list, read_tsv, .id = "enrichment_run")
+
+## Plot data
+p = enrich_df %>% 
+  filter(!is.na(gene_ci_lo)) %>% 
+  ## Choose expression outlier cut-offs to plot, e.g., (0, 1) for custom:
+  filter(expr_cut_off %in% c(2, 2.5, 3)) %>% 
+  ## Choose allele frequency cut-off to plot:
+  filter(af_cut_off %in% c(1e-4, 1e-3, 1e-2, 0.05)) %>% 
+  ## Choose TSS distance cut-off for plots:
+  filter(tss_cut_off %in% c(1e4)) %>% 
+  ## Choose enrichment run (file name without the txt ending)
+  filter(enrichment_run %in% c("utr5", "upstream", "exonic", "splicing", "intergenic", "intronic")) %>% 
+  ## convert the value you are coloring by to a factor
+  mutate(expr_cut_off = factor(expr_cut_off)) %>% 
+  ## pick x-axis: expr_cut_off, af_cut_off, tss_cut_off, enrichment_run:
+  ggplot(., aes(x = af_cut_off,
+                y = gene_or, ymax = gene_ci_hi, ymin = gene_ci_lo,
+  ## pick variable for COLORS: expr_cut_off, af_cut_off, tss_cut_off, enrichment_run
+                col = expr_cut_off
+                )) +
+  ## x-axis log scale only for af_cut_off:
+  scale_x_log10() +
+  geom_hline(yintercept = 1, col = "grey60") +
+  geom_pointrange(fatten = 1, show.legend = T, position = position_dodge(width = 0.3)) + 
+  theme_classic() +
+  ## Choose colors
+  scale_color_manual(values=wes_palette(n=5, name="Zissou1")[c(1,3,5)]) + ##
+  facet_wrap(~enrichment_run) +
+  xlab("Allele frequency cut-off") + ylab("Odds ratio")
+p
+
+## Save the plot
+filename = paste0(enrich_file_dir, "/exonic_enrichment_10kb.png")
+ggsave(filename, p, width = 3.5, height = 2.25) 
+
+
+  
+  
+
--- ore_bsub.sh
+++ ore_bsub.sh
@@ -26,7 +26,7 @@ VCF="$PARENT_DIR/wgs/ad_wgs_cp.vcf.gz"
 # ore_2018_06 ore_2018_05
 OUT_PREFIX="$PARENT_DIR/ore_2018_05/ad_ore"
 OUTLIER_OUT="$PARENT_DIR/ore_2018_05/most_extreme_outs_t36/ad_ore_outliers.txt"
-ENRICH_F="$PARENT_DIR/ore_2018_05/most_extreme_t36_enrich/ad_ore_exonic_synonymous_10kb.txt"
+ENRICH_F="$PARENT_DIR/ore_2018_05/most_extreme_t36_enrich/ad_ore_upstream_ens_ref_10kb.txt"
 
 cd /sc/orga/projects/chdiTrios/Felix/dna_rna/ore
 
@@ -43,14 +43,14 @@ time mprof run --include-children --multiprocess python -m ore.ore --vcf $VCF \
     --enrich_file $ENRICH_F \
     --distribution "normal" \
     --extrema \
-    --threshold 2 2.5 3 4 \
+    --threshold 2 2.25 2.5 2.75 3 4 \
     --af_rare 5e-2 1e-2 1e-3 1e-4 1e-5 \
     --intracohort_rare_ac 5 \
     --tss_dist 1e3 2e3 5e3 1e4 \
     --annovar \
     --refgene \
     --ensgene \
-    --variant_class "exonic" \
+    --variant_class "upstream" \
     --humandb_dir "/sc/orga/projects/chdiTrios/whole_genome/humandb" \
     --processes 5
 
@@ -64,6 +64,19 @@ mv ad_ore_all_data.txt ad_ore_all_data_exonic_synonymous_ref_ens_10kb.txt
 mv ad_ore_rv_w_outliers.txt ad_ore_rv_w_outliers_exonic_synonymous_ref_ens_10kb.txt 
 
 
+### RERUNNING
+
+cp ad_ore_all_data_splicing_ref_ens_10kb.txt ad_ore_all_data.txt 
+cp ad_ore_rv_w_outliers_splicing_ref_ens_10kb.txt ad_ore_rv_w_outliers.txt 
+
+cp ad_ore_all_data_upstream_ref_ens_10kb.txt ad_ore_all_data.txt 
+cp ad_ore_rv_w_outliers_upstream_ref_ens_10kb.txt ad_ore_rv_w_outliers.txt 
+
+cp ad_ore_all_data_utr5_ref_ens_10kb.txt ad_ore_all_data.txt 
+cp ad_ore_rv_w_outliers_utr5_ref_ens_10kb.txt ad_ore_rv_w_outliers.txt 
+
+cp ad_ore_all_data_exonic_ref_ens_10kb.txt ad_ore_all_data.txt 
+cp ad_ore_rv_w_outliers_exonic_ref_ens_10kb.txt ad_ore_rv_w_outliers.txt 
 
 
 # cd /sc/orga/projects/chdiTrios/Felix/alzheimers
