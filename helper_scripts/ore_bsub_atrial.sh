066e128d220d73fe1062c8f2167ced50607fdd92
frichter <frichter91@gmail.com>
2018-07-25 08:35:27 -0400

    updated column processing

--- helper_scripts/ore_bsub_atrial.sh
+++ helper_scripts/ore_bsub_atrial.sh
@@ -23,7 +23,7 @@ VCF="/sc/orga/projects/chdiTrios/Felix/dna_rna/wgs_pcgc_2018_01/wgs_atrial_ids.n
 EXPR_F="/sc/orga/projects/chdiTrios/Felix/rna/pcgc/expression_data_rpkm_cutoff/ns_atrial/residual_expr_5_SVs_hg19.bed.gz"
 OUT_PREFIX="$PARENT_DIR/atrial_ore"
 OUTLIER_OUT="$PARENT_DIR/atrial_ore_SV5_utliers_most_extreme.txt"
-ENRICH_F="$PARENT_DIR/atrial_ore_per_anno_10kb.txt"
+ENRICH_F="$PARENT_DIR/atrial_ore_per_anno_ref_ens_splicing_10kb.txt"
 
 cd /sc/orga/projects/chdiTrios/Felix/dna_rna/ore
 
@@ -45,6 +45,9 @@ python -m ore.ore --vcf $VCF \
     --intracohort_rare_ac 5 \
     --tss_dist 1e4 \
     --annovar \
+    --variant_class "splicing" \
+    --ensgene \
+    --refgene \
     --humandb_dir "/sc/orga/projects/chdiTrios/whole_genome/humandb" \
     --processes 3
 
