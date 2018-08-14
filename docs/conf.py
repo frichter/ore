bd68365ea80946010aff95366b99b4e4251561a8
frichter <frichter91@gmail.com>
2018-08-13 21:24:58 -0400

    trying to import version

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,9 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-# sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../'))
+
+print("testing print")
 
 
 # -- Project information -----------------------------------------------------
