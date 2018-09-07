ab807bf027d45ecaa776345e8340813ee2caccb1
frichter <frichter91@gmail.com>
2018-09-07 07:05:38 -0400

    recalc z-score

--- ore/ore.py
+++ ore/ore.py
@@ -24,7 +24,7 @@ from .outliers import Outliers
 from .join_dna_rna import JoinedVarExpr
 from .version import __version__
 
-"""# Profiling libraries:
+# """# Profiling libraries:
 import cProfile
 import pstats
 from memory_profiler import profile
@@ -35,7 +35,7 @@ from memory_profiler import profile
 # """
 
 
-# @profile
+@profile
 def associate_outliers(args):
     """Prepare and associate variants and outliers.
 
@@ -322,7 +322,7 @@ def main():
     parser._action_groups.append(optional)
     args = parser.parse_args()
     # uncomment this line only to switch between profiling and regular mode
-    """Time (and memory?) profiling commands:
+    # """Time (and memory?) profiling commands:
     cprof_cmd = ('associate_outliers(args)')
     OUT_FILE = (args.output + 'stats.out')
     print(OUT_FILE)
